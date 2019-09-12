172651d746f387e62e18099f34e2766d00d4d593
viveksharma <viveksharma@10.9.1.215>
2019-09-12 19:14:21 +0530

    program

--- src/sample/test/ds/tree/BinaryTree.java
+++ src/sample/test/ds/tree/BinaryTree.java
@@ -1,20 +1,37 @@
 package sample.test.ds.tree;
 
+import java.util.ArrayList;
+import java.util.Set;
+import java.util.TreeMap;
+
+import sample.test.top25geeks.Node;
+
 public class BinaryTree
 //I shoud use comparable
 {
 	
 	Node root;
 	
-	private  class Node{
-		Integer value;
-		Node left;
-		Node right;
-		 Node(Integer data){
-			 this.value = data;
-		 }
+//	private  class Node implements Comparable{
+//		Integer value;
+//		Node left;
+//		Node right;
+//		 Node(Integer data){
+//			 this.value = data;
+//		 }
+//		@Override
+//		public int compareTo(Object o) {
+//			// TODO Auto-generated method stub
+//			return 0;
+//		}
+//	}
+//	
+	public BinaryTree() {
+		
 	}
-	
+	public BinaryTree(Node root) { //you can use this method to create binary tree not binary search tree.
+		this.root =root;
+	}
 	
 	public void add(Integer data){
 		
@@ -99,5 +116,203 @@ public class BinaryTree
 	   postOrder(temp);
 		
 	}
+   
+   /*Level traversal or BFS Tree
+    * 			 (1)
+		    	/    \ 
+		   	(2)       (3)
+		   /   \      /   \ 
+		(4)    (5)   (6)   (7)
+		
+	Result : 1,2,3,4,5,6,7	
+    */
+   
+   public void levelTraversal() {
+	   
+	  ArrayList<Node> arr = new ArrayList<Node>();
+	 
+//	   this.root  = new Node(1);
+//		root.left = new Node(2);
+//		root.right = new Node(3);
+//		root.left.left = new Node(4);
+//		root.left.right = new Node(5);
+//		root.right.left = new Node(6);
+//		root.right.right = new Node(7);
+	   Node temp = root;
+	   while(temp != null) {
+		 System.out.println(temp.value);
+		 if(temp.left != null) {
+			 arr.add(arr.size(),temp.left);
+		 }
+		 if(temp.right != null) {
+			 arr.add(arr.size(),temp.right);
+		 }
+		 if(arr.size()==0) return;
+		 
+		 temp = arr.remove(0);
+	   }
+ }
+ /*
+  * Algo :
+  * 1. create an empty stack S.
+  * 2. Initialize current_node as root.
+  * 3. Push the current_node to stack S and set current_node = current_node->left
+  * 4.If current_node == NIL and S != NIL
+  *   (a) Pop the top element from stack into popped_item
+  *   (b) print the popped_item and  set current_node =  popped_item->right
+  *   (c) go to step 3
+  * 5. If current_node == NIL and S == NIL , EXIT 
+  */
+ public void inOrderWithStack() {
+	java.util.Stack <Node> stack = new java.util.Stack<Node>();
+	Node current_node = root,popped_node = null;
+	stack.add(current_node);
+	current_node = current_node.left;
+	while(current_node != null || !stack.isEmpty()) {
+		if(current_node != null && current_node != root)
+			{
+			stack.add(current_node);
+			current_node = current_node.left;
+			}
+		
+		if(current_node == null) {
+			popped_node = stack.remove(stack.size()-1);
+			System.out.println(popped_node.value);
+			current_node = popped_node.right;
+		}
+	}
+  }
+ 
+/*
+ * Left view of Binary Tree 
+ * https://www.geeksforgeeks.org/print-left-view-binary-tree/
+ * Input : 
+                 1
+               /   \
+              2     3
+             / \     \
+            4   5     6             
 
+   Output : 1 2 4
+
+Input :
+        1
+      /   \
+    2       3
+      \   
+        4  
+          \
+            5
+             \
+               6
+
+ Output :1 2 4 5 6
+ */
+ public void leftView() {
+	 Node node = this.root;
+	 leftViewOfTree(node,1);
+	 max_level = 0;
+ }
+ private static int max_level = 0; // it will be used to make sure whether right node  will be visible or not.
+ public void leftViewOfTree(Node node, int level) {
+	 
+	 if(node == null) return;
+	 
+	  if(max_level < level) {
+		System.out.println(node.value);
+		max_level = level;
+	  }
+	  leftViewOfTree(node.left, level+1);
+	  leftViewOfTree(node.right, level+1);
+ }
+ 
+ /*
+  * Top view of Binary tree
+  * 
+  * refer to vertical traversal  in the below example node 5,6 should not be printed for
+  * top view.
+  * 
+  * Approach: 
+  *  call vertical-traversal
+  *  if any vertical has 1+ node remove other node.
+  *  print the result.
+  */
+ 
+ public void topView() {
+	 TreeMap<Integer,ArrayList>  ht  = new TreeMap<Integer,ArrayList>();
+		
+		Node temp = this.root;
+		vertical(temp, 0 , ht);
+		Set<Integer> i = ht.keySet();
+	    for (int keys : i) {
+	        System.out.println(ht.get(keys).get(0));
+	    }
+		
+ }
+ 
+ 
+ 
+ 
+ /*
+  *  			 (1)
+		    	/    \ 
+		   	(2)       (3)
+		   /   \     /   \ 
+		(4)    (5)(6)   (7)
+		
+		
+		root = Level = 0
+		go left : level++
+		go righ : leve --
+		
+		
+		 		    (1)l=0
+		    l++	/          \ l--
+		   	 (2)l=1         (3) l=-1
+	    l++ /   \l--    l++ /   \ l--
+		(4)l=2   (5)l=0   (6)l=0   (7)l=-2
+		
+		
+		Result :  [4],[2],[1,5,6],[3],[7]
+		
+		Algo :
+		
+		 Do the preordertraversal.
+        Take a variable called level, when ever you go left, do level++ AND when ever you go right do level–.
+        With step above we have separated out the levels vertically.
+        Now you need to store the elements of each level, so create a TreeMap and the (key,value) pair will be (level,elements at that level).
+       At the end iterate through the TreeMap and print the results.
+		
+		
+  */
+ public void verticalOrder() {
+	TreeMap<Integer,ArrayList>  ht  = new TreeMap<Integer,ArrayList>();
+	
+	Node temp = this.root;
+	vertical(temp, 0 , ht);
+	Set<Integer> i = ht.keySet();
+    for (int keys : i) {
+        System.out.println(ht.get(keys));
+    }
+	
+ } 
+ 
+ private void vertical(Node n , int level,TreeMap<Integer,ArrayList>  ht ) {
+	 
+	 if(n== null) return;
+	 
+	 if(ht.containsKey(level)) {
+		 ArrayList a = ht.get(level);
+		 a.add(n.value);
+		 ht.put(level, a);
+	 }else {
+		 
+		 ArrayList a  = new ArrayList();
+		 a.add(n.value);
+		 ht.put(level,a);
+	 }
+	 vertical(n.left,level+1,ht);
+	 vertical(n.right,level-1,ht);
+	 
+ }
 }
--- src/sample/test/ds/tree/Main.java
+++ src/sample/test/ds/tree/Main.java
@@ -3,6 +3,8 @@ package sample.test.ds.tree;
 import java.util.ArrayList;
 import java.util.List;
 
+import sample.test.top25geeks.Node;
+
 public class Main {
     final List list;
     Main(){
@@ -27,6 +29,84 @@ public class Main {
 		
 		System.out.println("\n\n Post Order traversal");
 		btree.postOrderTraverse();
+		
+		Node root  = new Node(1);
+		root.left = new Node(2);
+		root.right = new Node(3);
+		root.left.left = new Node(4);
+		root.left.right = new Node(5);
+		root.right.left = new Node(6);
+		root.right.right = new Node(7);
+		
+		 btree = new BinaryTree(root);
+		 System.out.println("Level traversal");
+		 btree.levelTraversal();
+		 System.out.println("***Vertical Order Traversal\n ***");
+		 btree.verticalOrder();
+		 
+		 System.out.println("***Top View of BTree\n ***");
+		 btree.topView();
+		 System.out.println("InOrder traversal without Recurrsion");
+		 btree = new BinaryTree();
+		 btree.add(3);
+		 btree.add(4);
+		 btree.add(5);
+		 btree.add(1);
+		 btree.add(2);
+		 btree.add(6);
+		 btree.add(7);
+		 btree.inOrderWithStack();
+		 
+		 btree = new BinaryTree(root);
+		 System.out.println("Left View of BinaryTree");
+		 btree.leftView();
+		 
+		 /*
+		  * Create input like below 
+		  * Input :
+	        1
+	      /   \
+	    2       3
+	      \   
+	        4  
+	          \
+	            5
+	             \
+	               6
+		  */
+		 
+		    root  = new Node(1);
+			root.left = new Node(2);
+			root.right = new Node(3);
+			root.left.right = new Node(4);
+			root.left.right.right = new Node(5);
+			root.left.right.right.right = new Node(6);
+			//root.right.right = new Node(7);
+			
+			 btree = new BinaryTree(root);
+			 System.out.println("Left View of BinaryTree");
+			 btree.leftView();
+			 
+			 /*
+			  * Create input like below 
+			  * Input :
+			        10
+			      /   \
+			    20       30
+			            /
+			           (40)
+		    *Output:10 ,20 ,40
+		    */
+			 
+			 root  = new Node(10);
+			 root.left = new Node(20);
+			 root.right = new Node(30);
+			 root.right.left = new Node(40);
+			 btree = new BinaryTree(root);
+			 System.out.println("Left View of BinaryTree");
+			 btree.leftView();
+			 
 	}
+	
 
 }
